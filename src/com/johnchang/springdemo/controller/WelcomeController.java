f7a78e1bf63cf7bbcad9107fdb21bfaa11e9930a
John Chang <nkex606@gmail.com>
2017-09-14 20:14:42 +0800

    test

--- src/com/johnchang/springdemo/controller/WelcomeController.java
+++ src/com/johnchang/springdemo/controller/WelcomeController.java
@@ -18,7 +18,7 @@ public class WelcomeController {
 	@RequestMapping(value = "/")
 	public String doWelcome(Model model) {
 //		WelcomeService welcomeService = new WelcomeService();
-		List<String> welcomeMessage = welcomeService.getWelcomeMessage("John");
+		List<String> welcomeMessage = welcomeService.getWelcomeMessage("Elsa");
 
 		model.addAttribute("myWelcomeMessage", welcomeMessage);
 
