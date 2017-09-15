b200510573b54a7d7a86ffbfd0204322041d6069
John Chang <nkex606@gmail.com>
2017-09-16 00:51:28 +0800

    try try

--- scripts/tomcat_stop.sh
+++ scripts/tomcat_stop.sh
@@ -2,4 +2,6 @@
 
 service tomcat7 stop
 
-rm -r /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT/
+if [ -d /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT/ ]; then
+  rm -r /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT/
+fi
