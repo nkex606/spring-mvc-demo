f311bcec1241db42451a5c8cad7306bd5cb46202
John Chang <nkex606@gmail.com>
2017-09-22 12:58:39 +0800

    blue-green again

--- scripts/tomcat_stop.sh
+++ scripts/tomcat_stop.sh
@@ -5,3 +5,7 @@ service tomcat7 stop
 if [ -d /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT/ ]; then
   rm -r /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT/
 fi
+
+if [ -f /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT.war ]; then
+  rm /var/lib/tomcat7/webapps/spring-mvc-demo-0.0.1-SNAPSHOT.war
+fi
